6f0979fb0d3fc7b495bf5fe24b1c38137c253e32
Augusto Bizarria <165205881+augustbiza@users.noreply.github.com>
2024-06-22 18:44:52 -0300

    n, b, k

--- Lista 13 (Alocação Dinâmica)/Questão 03.c
+++ Lista 13 (Alocação Dinâmica)/Questão 03.c
@@ -10,12 +10,11 @@ void leiaVal (int * ptrN) {
 void teste(int val, int *pB, int *pK) {//ponteiros que apontam para o local em que os ponteiros da main apontam
     int menorB = val;
 
-    if(val >= 0) { //n positivo
-        for(int c = 0; c < val; c++) {//fixo o B e percorro o K e faço as multiplicacoes até ambos se igualarem ao valor de 'n'ou'val' 0x0,0x1,...nxn
+    if(val > 0) { //n positivo
+        for(int c = 1; c <= val; c++) {//fixo o B e percorro o K e faço as multiplicacoes até ambos se igualarem ao valor de 'n'ou'val' 1x1,...nxn
         *pB = c;
-        for(int c2 = 0; c2 < val; c2++) {
+        for(int c2 = 1; c2 <= val; c2++) {
             *pK = c2;
-
                 if((*pB) * (*pK) == val) {
                     printf("B = %d\nK = %d\n\n", *pB, *pK);
                     if(*pB < *pK && menorB > *pB) {
@@ -23,20 +22,33 @@ void teste(int val, int *pB, int *pK) {//ponteiros que apontam para o local em q
                     }
                 }
         }
+        
     }
     printf("Menor B = %d", menorB);
     }
 
-    if(val < 0) { //n negativo
-        for(int c = 0; c < val; c++) {
+    else if(val < 0) { //n negativo
+
+        for(int c = val; c < 0; c++) {
             *pB = -c;
-            for(int c2 = 0; c2 < val; c++) {
+            for(int c2 = val; c2 < 0; c2++) {
+                *pK = c2;
                 if((*pB) * (*pK) == val) {
                     printf("B = %d\nK = %d\n\n", *pB, *pK);
                 }
             }
         }
 
+        for(int c = val; c < 0; c++) {
+            *pB = c;
+            for(int c2 = val; c2 < 0; c2++) {
+                *pK = -c2;
+                if((*pB) * (*pK) == val) {
+                    printf("B = %d\nK = %d\n\n", *pB, *pK);
+                }
+            }
+        }
+    printf("Menor B = %d", menorB);
     }
     
 }
@@ -45,7 +57,7 @@ int main(void) {
 
     int n, b, k;
     
-    leiaVal(&n);//passagem por referência
+    leiaVal(&n);//passagem por referência(endereço de memoria de 'n')
 
     teste(n, &b, &k);//passagem: valor, referência, referência
 
