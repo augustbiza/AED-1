b00c04104d7e17da3aff6be8a0d245da75d70413
Augusto Bizarria <165205881+augustbiza@users.noreply.github.com>
2024-06-23 11:30:11 -0300

    !NÃO CONSEGUI FAZER!

--- Lista 13 (Alocação Dinâmica)/Questão 04.c
+++ Lista 13 (Alocação Dinâmica)/Questão 04.c
@@ -1,32 +1,29 @@
 // p1 = maior < 'm'  e  p2 = menor > 'm'
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void leiaVal(int *val) {
     printf("Informe o valor de 'm': ");
     scanf("%d", val);
 }
 
-void primo(int val, int *maiormenor, int *menormaior) {
+void primo(int val, int *maiormenor) {
 
-    int c;
-        for(c = 1; c < val; c++) {
-            if(val % c != 0) {*maiormenor = c;}
+    int numAnt, numProx;
 
-            if((val + c) % c != 0) {*menormaior = val + c;}
-        }
-        
-    printf("%d\n%d", *menormaior, *maiormenor);
+    for(int c = 2; c < )
+
 }
 
 int main(void) {
 
-    int m, p1, p2 = 0;
+    int m, p1;
     leiaVal(&m);
 
-    primo(m, &p1, &p2);
+    primo(m, &p1);
 
-    printf("%d\n%d", p1, p2);
+    printf("%d", p1);
 
     return 0;
 }
