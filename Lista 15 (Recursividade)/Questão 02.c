57f93a856aa2d38313c566495f6155b88035c1b7
Augusto Bizarria <165205881+augustbiza@users.noreply.github.com>
2024-06-23 19:40:35 -0300

    1234554321

--- Lista 15 (Recursividade)/Questão 02.c
+++ Lista 15 (Recursividade)/Questão 02.c
@@ -3,13 +3,11 @@
 void sequencia(int n) {
     printf("%d ", n);
 
-        if(n < 5) {
+        if(n < 5) {                  //instrução 1
             sequencia(n+1);
         }
 
-        printf("%d ", n);//--> chama a função sequencia(n-1) nao sei o porquê
-        //printf("%d ", n); 
-        //printf("%d ", n);
+        printf("%d ", n);            //instrução 2
 }
 
 int main(void) {
@@ -20,3 +18,35 @@ int main(void) {
 
     return 0;
 }
+
+//faz a instrução 1
+//TELA: 1
+// 1 < 5 --> chamada recursiva
+
+//faz a instrução 1 (segunda chamada)
+//TELA: 1 2
+// 2 < 5 --> chamada recursiva
+
+//...
+
+//faz a instrução 1 (quinta chamada)
+//TELA: 1 2 3 4 5
+// 5 < 5 --> FALSO
+//faz a instrução 2
+//TELA: 1 2 3 4 5 5
+
+//volta para a chamada 4
+//faz a instrução 2
+//TELA: 1 2 3 4 5 5 4
+
+//volta para a chamada 3
+//faz a instrução 2
+//TELA: 1 2 3 4 5 5 4 3
+
+//...
+
+//volta para a chamada 1
+//faz a instrução 2
+//TELA: 1 2 3 4 5 4 3 2 1
+
+//volta para a main
