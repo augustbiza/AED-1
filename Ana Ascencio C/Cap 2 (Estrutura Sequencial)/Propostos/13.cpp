1e9de834584e066a3c26838ef26a4f17f20c7679
Augusto Bizarria <165205881+augustbiza@users.noreply.github.com>
2024-07-21 23:17:20 -0300

    tabuada

---
+++ Ana Ascencio C/Cap 2 (Estrutura Sequencial)/Propostos/13.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+
+int main(void) {
+    system("cls");    
+
+    float num;
+    std::cout<<"Tabudada de qual numero? ";
+    std::cin>>num;
+
+    std::cout<<num<<"  ";
+    std::cout<<num*2<<"  ";
+    std::cout<<num*3<<"  ";
+    std::cout<<num*4<<"  ";
+    std::cout<<num*5<<"  ";
+    std::cout<<num*6<<"  ";
+    std::cout<<num*7<<"  ";
+    std::cout<<num*8<<"  ";
+    std::cout<<num*9<<"  ";
+    std::cout<<num*10<<std::endl;
+
+    return 0;
+}
